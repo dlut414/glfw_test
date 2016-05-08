3504e488e9b6cb2d5d3ac31b386248c9218d6306
dlut414 <hfy8373@gmail.com>
2016-05-08 22:25:03 +0900

    add stb_truetype test

--- glfw_testDll/glfw_testDll.cpp
+++ glfw_testDll/glfw_testDll.cpp
@@ -3,6 +3,8 @@
 
 #include <cstdio>
 #include "glfw_testDll.h"
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb_truetype.h"
 
 namespace glfw_testFunc {
 
@@ -33,8 +35,44 @@ namespace glfw_testFunc {
 		glfwTerminate();
 	}
 
+	#define BW  20
+	#define BH  79
+
+	unsigned char buffer[24 << 20];
+	unsigned char screen[BW][BH];
+
 	void glfw_test::mainLoop() {
 		while (!glfwWindowShouldClose(this->window)) {
+
+			///font staffs
+			stbtt_fontinfo font;
+			int i, j, ascent, baseline, ch = 0;
+			float scale, xpos = 2; // leave a little padding in case the character extends left
+			char *text = "ijijlij!"; // intentionally misspelled to show 'lj' brokenness
+
+			fread(buffer, 1, 1000000, fopen("c:/windows/fonts/arialbd.ttf", "rb"));
+			stbtt_InitFont(&font, buffer, 0);
+
+			scale = stbtt_ScaleForPixelHeight(&font, 15);
+			stbtt_GetFontVMetrics(&font, &ascent, 0, 0);
+			baseline = (int)(ascent*scale);
+
+			while (text[ch]) {
+				int advance, lsb, x0, y0, x1, y1;
+				float x_shift = xpos - (float)floor(xpos);
+				stbtt_GetCodepointHMetrics(&font, text[ch], &advance, &lsb);
+				stbtt_GetCodepointBitmapBoxSubpixel(&font, text[ch], scale, scale, x_shift, 0, &x0, &y0, &x1, &y1);
+				stbtt_MakeCodepointBitmapSubpixel(&font, &screen[baseline + y0][(int)xpos + x0], x1 - x0, y1 - y0, BH, scale, scale, x_shift, 0, text[ch]);
+				// note that this stomps the old data, so where character boxes overlap (e.g. 'lj') it's wrong
+				// because this API is really for baking character bitmaps into textures. if you want to render
+				// a sequence of characters, you really need to render each bitmap to a temp buffer, then
+				// "alpha blend" that into the working buffer
+				xpos += (advance * scale);
+				if (text[ch + 1])
+					xpos += scale*stbtt_GetCodepointKernAdvance(&font, text[ch], text[ch + 1]);
+				++ch;
+			}
+
 			int width, height;
 			glfwGetFramebufferSize(this->window, &width, &height);
 
@@ -46,14 +84,20 @@ namespace glfw_testFunc {
 			glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
 			glMatrixMode(GL_MODELVIEW);
 			glLoadIdentity();
-			glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-			glBegin(GL_TRIANGLES);
-			glColor3f(1.f, 0.f, 0.f);
-			glVertex3f(-0.6f, -0.4f, 0.f);
-			glColor3f(0.f, 1.f, 0.f);
-			glVertex3f(0.6f, -0.4f, 0.f);
-			glColor3f(0.f, 0.f, 1.f);
-			glVertex3f(0.f, 0.6f, 0.f);
+			//glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+			//glBegin(GL_TRIANGLES);
+			//glColor3f(1.f, 0.f, 0.f);
+			//glVertex3f(-0.6f, -0.4f, 0.f);
+			//glColor3f(0.f, 1.f, 0.f);
+			//glVertex3f(0.6f, -0.4f, 0.f);
+			//glColor3f(0.f, 0.f, 1.f);
+			//glVertex3f(0.f, 0.6f, 0.f);
+			//glEnd();
+			glBegin(GL_QUADS);
+				glVertex3f(-0.5f, -0.5f, 0.f);
+				glVertex3f(0.5f, -0.5f, 0.0f);
+				glVertex3f(0.5f, 0.5f, 0.0f);
+				glVertex3f(-0.5f, 0.5f, 0.0f);
 			glEnd();
 
 			glfwSwapBuffers(this->window);
