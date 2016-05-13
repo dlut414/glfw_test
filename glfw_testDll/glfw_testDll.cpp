d30c014418ee8a7e1bbac7e2f5e4b63e656f5030
dlut414 <hfy8373@gmail.com>
2016-05-13 21:33:30 +0900

    Major change

--- glfw_testDll/glfw_testDll.cpp
+++ glfw_testDll/glfw_testDll.cpp
@@ -35,20 +35,21 @@ namespace glfw_testFunc {
 		glfwTerminate();
 	}
 
-	#define BW  20
-	#define BH  79
+	#define BW  64
+	#define BH  64
 
 	unsigned char buffer[24 << 20];
-	unsigned char screen[BW][BH];
+	unsigned char screen[BH][BW];
+	unsigned char screen_processed[BH][BW][4];
 
 	void glfw_test::mainLoop() {
 		while (!glfwWindowShouldClose(this->window)) {
 
 			///font staffs
 			stbtt_fontinfo font;
-			int i, j, ascent, baseline, ch = 0;
+			int ascent, baseline, ch = 0;
 			float scale, xpos = 2; // leave a little padding in case the character extends left
-			char *text = "ijijlij!"; // intentionally misspelled to show 'lj' brokenness
+			char *text = "A10BDC"; // intentionally misspelled to show 'lj' brokenness
 
 			fread(buffer, 1, 1000000, fopen("c:/windows/fonts/arialbd.ttf", "rb"));
 			stbtt_InitFont(&font, buffer, 0);
@@ -62,7 +63,7 @@ namespace glfw_testFunc {
 				float x_shift = xpos - (float)floor(xpos);
 				stbtt_GetCodepointHMetrics(&font, text[ch], &advance, &lsb);
 				stbtt_GetCodepointBitmapBoxSubpixel(&font, text[ch], scale, scale, x_shift, 0, &x0, &y0, &x1, &y1);
-				stbtt_MakeCodepointBitmapSubpixel(&font, &screen[baseline + y0][(int)xpos + x0], x1 - x0, y1 - y0, BH, scale, scale, x_shift, 0, text[ch]);
+				stbtt_MakeCodepointBitmapSubpixel(&font, &screen[baseline + y0][(int)xpos + x0], x1 - x0, y1 - y0, BW, scale, scale, x_shift, 0, text[ch]);
 				// note that this stomps the old data, so where character boxes overlap (e.g. 'lj') it's wrong
 				// because this API is really for baking character bitmaps into textures. if you want to render
 				// a sequence of characters, you really need to render each bitmap to a temp buffer, then
@@ -71,6 +72,16 @@ namespace glfw_testFunc {
 				if (text[ch + 1])
 					xpos += scale*stbtt_GetCodepointKernAdvance(&font, text[ch], text[ch + 1]);
 				++ch;
+			}
+			for (int i = 0; i < BH; ++i) {
+				for (int j = 0; j < BW; ++j) {
+					putchar(" .:ioVM@"[screen[i][j] >> 5]);
+					screen_processed[i][j][3] = screen[BW - i - 1][j];
+					screen_processed[i][j][0] = 0;
+					screen_processed[i][j][1] = 0;
+					screen_processed[i][j][2] = 0;
+				}
+				putchar('\n');
 			}
 
 			int width, height;
@@ -93,12 +104,31 @@ namespace glfw_testFunc {
 			//glColor3f(0.f, 0.f, 1.f);
 			//glVertex3f(0.f, 0.6f, 0.f);
 			//glEnd();
+			glEnable(GL_TEXTURE_2D);
+			glEnable(GL_BLEND);
+			//glDisable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glClearColor(1, 1, 1, 0);
+			GLuint texid;
+			glGenTextures(1, &texid);
+			glBindTexture(GL_TEXTURE_2D, texid);
+		 // glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, BW, BH, 0, GL_RGBA, GL_UNSIGNED_BYTE, screen_processed);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 			glBegin(GL_QUADS);
-				glVertex3f(-0.5f, -0.5f, 0.f);
+				glTexCoord3f(0.0f, 0.0f, 0.0f);
+				glVertex3f(-0.5f, -0.5f, 0.0f);
+				glTexCoord3f(1.0f, 0.0f, 0.0f);
 				glVertex3f(0.5f, -0.5f, 0.0f);
+				glTexCoord3f(1.0f, 1.0f, 0.0f);
 				glVertex3f(0.5f, 0.5f, 0.0f);
+				glTexCoord3f(0.0f, 1.0f, 0.0f);
 				glVertex3f(-0.5f, 0.5f, 0.0f);
 			glEnd();
+			glBindTexture(GL_TEXTURE_2D, 0);
 
 			glfwSwapBuffers(this->window);
 			glfwPollEvents();
